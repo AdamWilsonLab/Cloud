583b2fb462064006ae5bcdee053168d5e30695d6
Adam M. Wilson <adammichaelwilson@gmail.com>
2014-07-01 13:51:40 -0400

    moved much Sm material back to word doc
    Former-commit-id: 5675311ec835be5140bf4d0f925c7b1d645011eb
    Former-commit-id: ddce8ce042edcfb5211ad234449937e35fcfc823
    Former-commit-id: 0e8e6da1ae0abf5160d4241f751b905a46276891

--- analysis/6_LoadData.R
+++ analysis/6_LoadData.R
@@ -1,13 +1,6 @@
 ## File to load data for use in SM.Rnw and 6_Figures
 
 
-### Load data
-print("Loading full raster layers")
-cf_mean=readAll(raster("data/MCD09_deriv/MCD09_meanannual.tif"))
-cf_visseas=readAll(raster("data/MCD09_deriv/seas_visct.tif"))
-inter=readAll(raster("data/MCD09_deriv/inter.tif"))
-intra=readAll(raster("data/MCD09_deriv/intra.tif"))
-NAvalue(inter)=0
 
 ### Load validation data
 cldm=read.csv("data/validation/cldm.csv",row.names=NULL)
@@ -46,14 +39,14 @@ cldm$difm=cldm$MCD09_meanb16-cldm$cld
 nstation_all=length(unique(cldm$StaID[!is.na(cldm$cld_all)]))
 nstation_mod=length(unique(cldm$StaID[!is.na(cldm$cld)]))
 
-# approximate size of M*D09GA archive - get total size for one day from the USGS website
-size=as.numeric(
-  sub("M","",
-      grep("[0-9]*M$",
-           scan("http://e4ftl01.cr.usgs.gov/MOLT/MOD09GA.005/2000.04.30/",what="char"),
-           value=T)))
 ## extract all filesizes in MB (all the HDFs) and sum them and covert to TB for the length of the full record
 if(!file.exists("data/out/SizeOfMOD09GAArchive.Rdata")){
+  # approximate size of M*D09GA archive - get total size for one day from the USGS website
+  size=as.numeric(
+    sub("M","",
+        grep("[0-9]*M$",
+             scan("http://e4ftl01.cr.usgs.gov/MOLT/MOD09GA.005/2000.04.30/",what="char"),
+             value=T)))
   tsize=sum(size)/1024/1024*as.numeric(as.Date("2014-03-31")-as.Date("2000-02-24"))*2
   save(tsize,file="data/out/SizeOfMOD09GAArchive.Rdata")
 }
@@ -63,3 +56,22 @@ load("data/out/SizeOfMOD09GAArchive.Rdata")
 #hpd_resid=HPDinterval(as.mcmc(cldm$resid))#,c(0.025,0.25,0.5,0.75,0.975),na.rm=T)
 #hpd_cld=HPDinterval(as.mcmc(cldm$difm))#,c(0.025,0.25,0.5,0.75,0.975),na.rm=T)
 #hpd_cld_all=HPDinterval(as.mcmc(cldm$difm_all))#,c(0.025,0.25,0.5,0.75,0.975),na.rm=T)
+
+### Load data
+if( Sys.info()["nodename"]=="repens"){
+#  cf_mean=raster("data/MCD09_deriv/MCD09_meanannual.tif")
+#  cf_visseas=raster("data/MCD09_deriv/seas_visct.tif")
+#  inter=raster("data/MCD09_deriv/inter.tif")
+#  intra=raster("data/MCD09_deriv/intra.tif")
+#  NAvalue(inter)=0
+}
+
+if( Sys.info()["nodename"]=="litoria"){  
+  print("Loading full raster layers")
+  cf_mean=readAll(raster("data/MCD09_deriv/MCD09_meanannual.tif"))
+  cf_visseas=readAll(raster("data/MCD09_deriv/seas_visct.tif"))
+  inter=readAll(raster("data/MCD09_deriv/inter.tif"))
+  intra=readAll(raster("data/MCD09_deriv/intra.tif"))
+  NAvalue(inter)=0
+}
+
