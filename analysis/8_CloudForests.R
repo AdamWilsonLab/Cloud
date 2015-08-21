3a2be20889131e0f33d769fdb42cac6335db6daa
Adam M. Wilson <adam.wilson@yale.edu>
2015-08-21 13:52:09 -0400

    add worldclim data to cloud forest model
    Former-commit-id: 0742cc82531910ad21dc6ac90465270d269a7148
    Former-commit-id: 7c645849cccf211d9ce44b236fca750ec1b44a39
    Former-commit-id: ce1d2f6fc2cef70785822b2a0a309cf8ecd81661

--- analysis/8_CloudForests.R
+++ analysis/8_CloudForests.R
@@ -7,7 +7,12 @@
 library(dplyr)
 library(AUC)
 library(texreg)
+library(visreg)
 library(raster)
+
+library(doParallel)
+registerDoParallel(5)
+
 cfp=readOGR("data/src//cloud_forests/","cloud_forest_points_1997")
 
 bprods=c("data/MCD09_deriv/inter.tif",
@@ -20,7 +25,18 @@ bprods=c("data/MCD09_deriv/inter.tif",
 if(!file.exists("data/out/CloudEnv.tif")){
 
 env=crop(stack(bprods),extent(c(90,171,-14,20)))
-env2=stack(env,crop(raster("data/worldclim_alt.tif"),env))
+env=crop(stack(bprods),extent(c(-160,160,-23.43726,23.43726))) ## global
+
+wc=crop(stack(c(
+  "/mnt/data/jetzlab/Data/environ/global/worldclim/alt.bil",
+  "/mnt/data/jetzlab/Data/environ/global/worldclim/bio_1.bil",
+  "/mnt/data/jetzlab/Data/environ/global/worldclim/bio_12.bil",
+  "/mnt/data/jetzlab/Data/environ/global/worldclim/bio_15.bil"
+)),env)
+
+env2=stack(env,wc)
+
+
 ## load land map
 land=map(interior=F,fill=T,xlim=bbox(env2)[1,],ylim=bbox(env2)[2,],plot=F)
 land=map2SpatialPolygons(land,IDs=1:length(land$names))
@@ -42,26 +58,32 @@ abs$ID=1:nrow(abs)
 
 data=bind_rows(pres,abs)
 
+## add flag for region
+data$region=as.factor(ifelse(data$x<(-29.5),"Americas",ifelse(data$x<63.4,"Africa","Asia Pacific")))
+
 save(data,file="data/out/CloudForestPoints.Rdata")
 
 
 ################################################
 load("data/out/CloudForestPoints.Rdata")
-env=stack("data/out/CloudEnv.tif")
+env=stack("data/out/CloudEnv_scaled.tif")
 names(env)=sub("[.]tif","",c(basename(bprods),"elev"))
 
 
 models=rbind.data.frame(
   c("elevation","cf ~ elev+I(elev^2)"),
   c("cloud+elevation","cf ~ inter+I(inter^2)+elev+I(elev^2)+intra+I(intra^2)+meanannual+I(meanannual^2)"),
-  c("justcloud","cf ~ inter+I(inter^2)+intra+I(intra^2)+meanannual+I(meanannual^2)"))
-)
+  c("all","cf ~ elev+I(elev^2)+inter+I(inter^2)+intra+I(intra^2)+meanannual+I(meanannual^2)+inter*intra*meanannual+region"),
+  c("justcloud","cf ~ inter+I(inter^2)+intra+I(intra^2)+meanannual+I(meanannual^2)"),
+  c("step","StepwiseSelection"))
+
 colnames(models)=c("name","formula")
 
-mods=foreach(f=models$formula) %do%{
-  glm(as.formula(as.character(f)), family=poisson(link=log),data=data)
+mods=foreach(f=models$formula[-nrow(models)]) %dopar%{
+  glm(as.formula(as.character(f)), family=binomial,data=data,weights=1E3^(1-cf))
 }
 
+mods[[nrow(models)]]=step(mods[[3]])
 
 models$AIC=unlist(lapply(mods,function(x) AIC(x)))
 models$AUC=unlist(lapply(mods,function(x) {
@@ -72,6 +94,10 @@ models$AUC=unlist(lapply(mods,function(x) {
 names(mods)=models$name
 
 screenreg(mods)
+
+visreg(mods$step)
+visreg2d(mods$step,"meanannual","intra")
+visreg(mods$step,xvar="meanannual","intra")
 
 #beginCluster(10)
 mcoptions <- list(preschedule=FALSE, set.seed=FALSE)
@@ -87,7 +113,7 @@ ps=foreach(i=1:nrow(models),.options.multicore=mcoptions,.combine=stack)%dopar%{
   raster(fo)
   }
 
-ps=stack(list.files("data/out/",pattern="CloudForestPrediction.*tif",full=T))
+ps=stack(list.files("data/out/",pattern="CloudForestPrediction_step.tif",full=T))
 
 psd=ps[[1]]-ps[[2]]
 
@@ -97,16 +123,18 @@ hcols=function(x,bias=1) {
 
 p_p1=
   
-  gplot(ps,max=5e5)+geom_raster(aes(fill=value))+facet_wrap("variable",ncol=1)+
-  scale_fill_gradientn(colours=hcols(100,bias=.3),trans = "log10",
+  gplot(ps,max=5e6)+
+  geom_raster(aes(fill=value))+facet_wrap("variable",ncol=1)+
+  scale_fill_gradientn(colours=hcols(1000,bias=.2),trans = "log10",#lim=c(1e-3,1),
                        name="Relative Occurrence Rate\np(x|Y=1)",na.value="transparent")+
-  coord_equal(xlim=c(90,160))+
-  #geom_polygon(aes(x=long,y=lat,group=group),
-  #             data=fortify(range),
-  #             fill="transparent",col="black",size=.2)+
+  coord_equal(xlim=c(-100,160))+
+  #coord_equal(xlim=c(90,160))+
+#  geom_polygon(aes(x=long,y=lat,group=group),
+#               data=fortify(land),
+#               fill="transparent",col="black",size=.2)+
   geom_point(aes(x = x, y = y), 
              data = data[data$cf==1,],
-             col="black",size=2)
+             col="black",size=1,solid = FALSE)
 
 gplot(psd,max=1e5)+geom_raster(aes(fill=value))+
   scale_fill_gradient2(low = "red", mid = "white", high = "blue",
